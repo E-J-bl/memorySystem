749855d1668704fe7b011e7ff89f5bee22d19a7d
Ethan <ethben0809@highgateschool.org.uk>
2025-02-06 23:48:23 +0000

    Broke malloc_under_128 the code up into nicer funcs. Lets me finally get malloc over 128 into some kind of working state, still missing that final stage of flipping the bits in the free table but this was done after a long day and was committed at midnight. For now this is beyond what this brain, which feels like it has been smashed between two golden bricks, can handle.

--- src/memory.rs
+++ src/memory.rs
@@ -53,52 +53,58 @@ impl Memory{
 
         value
     }
+    fn loop_over_two_buffers(num_addresses:u32, reg_0:u128, reg_1:u128) -> u32 {
+        let mut found_space=false;
+        let mut buff_one:u128= 2_u128.pow(128-num_addresses)-1;
+        let mut buff_two:u128= u128::MAX;
+        let mut offset:u32=0;
+        while !found_space{
+            if (reg_0 & !buff_one== 0) & (reg_1& !buff_two ==0){
+                found_space=true;
+            }else {
+                offset+=1;
+                if offset>127{
+                    return 129;
+                }
+                else if (num_addresses  +offset)<=128{
+                    buff_one=buff_one/2+Self::left_shift_until_msb(2_u128.pow(offset));
+                } else {
+                    buff_two = !(u128::MAX-(2_u128.pow(256 - offset - num_addresses)-1)) ;
+                    buff_one=Self::left_shift_until_msb(2_u128.pow(offset) - 1);
+
+                }
+            }
+        }
+        offset
+    }
+
+
     fn mall_under_128(&mut self, num_addresses:u32) -> u32 {
-        let mut return_address:u32=u32::MAX;
+
         let mut offset:u32=0;
         let mut cur_addr:u32=0;
         let mut found_space=false;
         let mut reg_0: u128 = *self.free_table.get(&cur_addr).unwrap();
         let mut reg_1: u128= *self.free_table.get(&(cur_addr + 1)).unwrap();
-        let mut buff_one:u128= 2_u128.pow(128-num_addresses)-1;
-        let mut buff_two:u128= u128::MAX;
+        let mut check_regs:u32=0;
+
+
         while !found_space{
             //println!("{},{},{:0128b},{:0128b},{:0128b},{:0128b}",offset,256 - offset - num_addresses,reg_0,!buff_one,reg_1,!buff_two);
             if &cur_addr>= (self.free_table.iter().max().unwrap().0) {
                 found_space=true;
                 continue
             }
+            check_regs= Self::loop_over_two_buffers(num_addresses,reg_0,reg_1);
 
-
-            if (reg_0 & !buff_one== 0) & (reg_1& !buff_two ==0){
-
+            if check_regs== 129{
+                cur_addr+=1;
+                reg_0=reg_1;
+                reg_1=*self.free_table.get(&(cur_addr + 1)).unwrap();
+            }
+            else {
+                offset=check_regs;
                 found_space=true;
-                return_address=128*cur_addr+offset;
-
-
-            }else {
-                offset+=1;
-                if offset>127{
-                    cur_addr+=1;
-                    offset=0;
-                    reg_0=reg_1;
-                    reg_1=*self.free_table.get(&(cur_addr+1)).unwrap();
-                    buff_one=2_u128.pow(128-num_addresses)-1;
-                    buff_two=u128::MAX;
-
-                }
-
-
-                else if (num_addresses  +offset)<=128{
-
-                    buff_one=buff_one/2+Self::left_shift_until_msb(2_u128.pow(offset));
-
-                } else {
-                    buff_two = !(u128::MAX-(2_u128.pow(256 - offset - num_addresses)-1)) ;
-                    buff_one=Self::left_shift_until_msb(2_u128.pow(offset) - 1);
-
-                }
-                // somewhere here is a bug that gets me stuck in a loop the second that the address ticks up by 1
             }
 
         }
@@ -116,68 +122,71 @@ impl Memory{
             self.free_table.insert(cur_addr, r1);
         }else{
 
-            let r1=reg_0 +(2_u128.pow(num_addresses) - 1)/2_u128.pow(offset);
+            let r1=reg_0 +(2_u128.pow(num_addresses) - 1)/2_u128.pow(offset-1);
             let r2= reg_1+Self::left_shift_until_msb(2_u128.pow(offset+num_addresses-128));
             self.free_table.insert(cur_addr,r1);
 
             self.free_table.insert(cur_addr+1,r2);
         }
-        return_address
+        offset +128*cur_addr
     }
 
 
 
 
-    fn mall_over_128(&mut self, num_addresses:u16) -> (u32, u32) {
-        let mut count:u128=0;
+    fn mall_over_128(&mut self, num_addresses:u32) -> u32 {
+        let mut offset:u32=0;
         let mut found:bool=false;
         let mut cur_addr:u32=0;
-        let contig_min:u32= ((num_addresses / 128) - 1) as u32;
-        let mut found_min_cont=false;
+        let contig_min:u32= ((num_addresses / 128) - 1);
+        let mut found_min_cont=true;
+        let mut check_regs:u32=0;
+
         while !found{
-            if self.free_table.get(&cur_addr)!=None{
-                found_min_cont=true;
-                for i  in 0..contig_min{
-                    if self.free_table.get(&(cur_addr+(i))).unwrap()!= &u128::MIN{
-                        found_min_cont=false;
-                    } else {
-
-                    }
-                if found_min_cont{
-
-                    let before= self.free_table.get(&(cur_addr - 1)).unwrap().trailing_zeros();
-                    let after= self.free_table.get(&(cur_addr+contig_min+1)).unwrap().leading_zeros();
-
-                    if before+after+128*contig_min>=num_addresses as u32 {
-                        self.free_table.insert(cur_addr-1, match self.free_table.get(&(cur_addr - 1)) {
-                            Some(x) => x,
-                            None => panic!(),
-                        } & (2 ** &before - 1)as u128);
-                        for i in 0..contig_min{
-                            self.free_table.insert(cur_addr+i,u128::MAX);
-                        };
-                        self.free_table.insert(cur_addr+contig_min+1,self.free_table.get(&(cur_addr+contig_min+1)).unwrap()& Self::left_shift_until_msb((2 * *&after - 1) as u128));
-                        return (cur_addr*128-before,(cur_addr+contig_min)*128+after);
-
-                        //need to change so it does not just fill the last address it only fills as much as it needs;
-
-                    }
-
-
-
-                    }
-                }
-            }else{
-                cur_addr+=1
+            if &cur_addr>= (self.free_table.iter().max().unwrap().0) {
+                found=true;
+                continue
             }
-            if cur_addr> *(match self.look_up.keys().max() {
-                None => panic!(),
-                Some(x) => x
-            }) {
-                found=true
-            } else {}
+            for i in 0..contig_min{
+                if self.free_table.get(&(cur_addr + i)).unwrap()!=&0{
+                    cur_addr+=1;
+                    found_min_cont=false;
+                    break
+                }
+            }
+            if !found_min_cont{
+                continue
+            }
+            if cur_addr!=0{
+                check_regs= Self::loop_over_two_buffers(num_addresses-128*contig_min,
+                                                        *self.free_table.get(&cur_addr).unwrap(),
+                                                        *self.free_table.get(&(cur_addr+contig_min+1)).unwrap())
+                ;
+                if check_regs== 129{
+                    cur_addr+=1;
+                }
+                else {
+                    offset=check_regs;
+                    found=true;
+                }
+            }
+            else {
+                if !*self.free_table.get(&(cur_addr+contig_min+1)).unwrap() & Self::left_shift_until_msb((num_addresses - 128 * contig_min) as u128){
+                    found=true;
+                    offset=0;
+                }
+                else {
+                    cur_addr+=1;
+                }
+            }
         }
-        (0,0)
+
+        //to add
+        // ticks the areas that are being allocated over
+
+
+
+        cur_addr*128 +offset
 
     }
 
@@ -192,6 +201,7 @@ impl Memory{
 
         if num_addresses<128{
             let st=self.mall_under_128((num_addresses+1) as u32);
+            println!("{} {}",num_addresses,st);
             let key:u128= SystemTime::now().duration_since(UNIX_EPOCH).expect("error with the time").as_nanos() ;
             self.flash.insert(st,key);
             (key,st,st+num_addresses as u32)
